a071ef73a6c193361e018feb31d1022984dc0491
Chris Issaris <61369849+limikis@users.noreply.github.com>
2020-02-23 14:41:15 +0200

    1

---
+++ 1.py
@@ -0,0 +1,20 @@
+with open("1.txt","r") as f:
+        data =f.read().split()
+f.close()
+x=len(data)
+for i in range(0,x):
+    pos=i
+    for j in range(pos+1,x):
+        if (len(data[pos])<len(data[j])):
+            pos=j
+    data[i],data[pos]=data[pos],data[i]
+a=0
+vowels = ('a', 'e', 'i', 'o', 'u','A','E','I','O','U','.')  
+for q in data[0:5]:
+    for i in q:
+       if i in vowels: 
+        q = q.replace(i, "")
+    data[a]=q[::-1]
+    a=a+1
+        
+print (data[0:4])
