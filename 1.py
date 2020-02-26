73bd29c5d2a78c57ebad2af1abe08c5eeac29a7a
Chris Issaris <61369849+limikis@users.noreply.github.com>
2020-02-26 02:42:20 +0200

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,6 +1,7 @@
 with open("1.txt","r") as f:
         data =f.read().split()
 f.close()
+print (data)
 x=len(data)
 for i in range(0,x):
     pos=i
@@ -9,6 +10,7 @@ for i in range(0,x):
             pos=j
     data[i],data[pos]=data[pos],data[i]
 a=0
+print ("oi 5 megaluteres:",data[0:4])
 vowels = ('a', 'e', 'i', 'o', 'u','A','E','I','O','U','.')  
 for q in data[0:5]:
     for i in q:
